3d72af3449cbecdf1194daebfab1fc71116407a4
Amine Lemaizi <40530011+aminelemaizi@users.noreply.github.com>
2024-10-19 15:12:21 +0100

    updating types for tests

--- Bricks/textarea_entry.py
+++ Bricks/textarea_entry.py
@@ -1,16 +1,15 @@
 from coded_flows.types import Str
 
 
-
 coded_flows_metadata = {
     "display_name": "Multiline Text",
     "description": "Multiline textual input that you can use as a manual entry",
     "icon": "keyboard",
     "type": "entry",
-    "entry_type": "textarea"
-
+    "entry_type": "textarea",
 }
 
-def textarea_entry()-> Str:
+
+def textarea_entry() -> Str:
     textarea_entry = ""
     return textarea_entry
